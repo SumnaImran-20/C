e56eed8a6d2ff7a130b48144960a15d814fdb57f
Sumna Imran <88527179+SumnaImran-20@users.noreply.github.com>
2021-10-01 23:40:12 +0500

    Add files via upload

---
+++ code12.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main ()
+{
+int number, digit, total;
+printf("---Sum of any 5 digit number---\n");
+printf ("Enter any 5 digit number: ");
+scanf ("%d", &number);
+ 
+digit = number%10;
+printf("last digit is %d.\n",digit);
+total = digit;
+
+digit = (number/10) % 10;
+printf("second last is %d.\n",digit);
+total = total + digit;
+
+digit = (number/100) % 10;
+printf("third digit is %d.\n",digit);
+total = total + digit;
+
+digit = (number/1000) % 10;
+printf("second digit is %d.\n",digit);
+total = total + digit;
+
+digit = (number/10000 % 10);
+printf("first digit is %d.\n",digit);
+total = total + digit;
+
+printf("sum of 5 digit is: %d",total);
+
+return 0;
+}
